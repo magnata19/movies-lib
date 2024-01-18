b8698152c8fa65a659f1946f27b2f89faf1b7753
Davidson <dehpacifico@gmail.com>
2024-01-17 22:37:56 -0300

    mostrando filmes na home

--- movies-lib/src/pages/Home.jsx
+++ movies-lib/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY
@@ -10,16 +11,27 @@ const Home = () => {
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+
+    setTopMovies(data.results)
   }
 
   useEffect(() => {
-    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
-    console.log(topRatedUrl)
-    getTopRatedMovies(topRatedUrl)
+
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`
+    getTopRatedMovies(topRatedURL)
+
   }, [])
+  
   return (
-    <div>Home</div>
+    <div className="container">
+      <h2 className="title">Melhores filmes: </h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie}/>
+        ))}
+      </div>
+    </div>
   )
 }
 

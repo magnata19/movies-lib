fa85efb4113fac21095b3ba6ad41c0be9956bd79
Davidson <dehpacifico@gmail.com>
2024-01-18 21:35:18 -0300

    criando pagina de busca por titulo

--- movies-lib/src/components/Navbar.jsx
+++ movies-lib/src/components/Navbar.jsx
@@ -1,18 +1,42 @@
-import './Navbar.css'
-import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
 
-import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { useState } from "react";
+
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch("");
+  }
+
   return (
     <div>
       <nav id="navbar">
         <h2>
-          <Link to="/"><BiCameraMovie />MoviesLib</Link>
+          <Link to="/">
+            <BiCameraMovie />
+            MoviesLib
+          </Link>
         </h2>
-        <form>
-          <input type="text" placeholder="Busque um filme" />
-          <button type='submit'><BiSearchAlt2 /></button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busque um filme"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
         </form>
       </nav>
     </div>
